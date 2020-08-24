8b8849e826c4c09d324f4ec2d298c6b9b305223d
apopeye <70023309+apopeye@users.noreply.github.com>
2020-08-24 00:37:07 -0300

    Update detail.php

--- detail.php
+++ detail.php
@@ -195,15 +195,7 @@ $preference->save();
 
     <a href="<?php echo $preference->init_point; ?>">Pagar la compra</a>
 
-<!--
-				<form action="/procesar-pago" method="POST">
-				  <script
-				   src="https://www.mercadopago.com.ar/integrations/v1/web-payment-checkout.js"
-				   data-preference-id="<?php echo $preference->id; ?>">
-				  </script>
-				</form>				
--->				
-				
+
                                 </div>
                             </div>
                         </div>
